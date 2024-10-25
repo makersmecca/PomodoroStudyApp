08f18860fce9f729478351778be9b9a5aaed46ed
Ayudh <ayudhnandi@gmail.com>
2024-10-25 14:08:09 +0530

    Stats component

---
+++ src/Components/Stats.jsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+
+const Stats = () => {
+  return (
+    <>
+      <h3>Stats</h3>
+      <button>
+        <Link to="/">Home</Link>
+      </button>
+    </>
+  );
+};
+
+export default Stats;
