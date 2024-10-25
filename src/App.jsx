3df32e5d7909abea2c3b131f1f32db2a1e421a46
Ayudh <ayudhnandi@gmail.com>
2024-10-25 11:59:42 +0530

    Added react Routes to switch between different components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import CustomTimer from "./Components/CustomTimer";
 import CountdownTimer from "./Components/CountdownTimer";
+import { Route, Routes } from "react-router-dom";
 function App() {
   return (
-    <>
-      <CustomTimer />
-      <CountdownTimer />
-    </>
+    <Routes>
+      <Route path="/" element={<CountdownTimer />} />
+      <Route path="/customtimer" element={<CustomTimer />} />
+    </Routes>
   );
 }
 
