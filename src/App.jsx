97301dbd19da04f5a15cf96ffdcaf2c47ddb1d09
Ayudh <ayudhnandi@gmail.com>
2024-10-25 12:51:41 +0530

    added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import CustomTimer from "./Components/CustomTimer";
 import CountdownTimer from "./Components/CountdownTimer";
 import RestTimer from "./Components/RestTimer";
+import Breathe from "./Components/Breathe";
+import Todo from "./Components/ToDo";
+
 import { Route, Routes } from "react-router-dom";
 function App() {
   return (
@@ -8,6 +11,8 @@ function App() {
       <Route path="/" element={<CountdownTimer />} />
       <Route path="/customtimer" element={<CustomTimer />} />
       <Route path="/rest" element={<RestTimer />} />
+      <Route path="/breathe" element={<Breathe />} />
+      <Route path="/todo" element={<Todo />} />
     </Routes>
   );
 }
