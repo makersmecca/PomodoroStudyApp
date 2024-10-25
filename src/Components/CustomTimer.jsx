028b053da012111a347574a2892baf5b7b322741
Ayudh <ayudhnandi@gmail.com>
2024-10-25 12:01:49 +0530

    added link to Countdown timer component using react router

--- src/Components/CustomTimer.jsx
+++ src/Components/CustomTimer.jsx
@@ -1,55 +1,5 @@
-// import { useEffect, useState } from "react";
-
-// const Timer = () => {
-//   const [seconds, setSeconds] = useState(0);
-//   const [minutes, setMinutes] = useState(0);
-//   const [startTimer, setStartTimer] = useState(false);
-//   const [resetTimer, setResetTimer] = useState(false);
-
-//   const timerStatus = () => {
-//     setStartTimer(() => !startTimer);
-//   };
-
-//   const resetTimerStatus = () => {
-//     console.log("reset timer function");
-//     setResetTimer(() => !resetTimer);
-//   };
-
-//   useEffect(() => {
-//     console.log("reset timer use effect");
-//     startTimer && setStartTimer(false);
-//     setSeconds(0);
-//     setMinutes(0);
-//     setStartTimer(false);
-//   }, [resetTimer]);
-
-//   useEffect(() => {
-//     if (startTimer) {
-//       if (seconds < 60) {
-//         setTimeout(() => {
-//           setSeconds(() => seconds + 1);
-//         }, 1000);
-//       } else {
-//         setMinutes(() => minutes + 1);
-//         setSeconds(0);
-//       }
-//     }
-//   });
-
-//   return (
-//     <div>
-//       <div>
-//         {minutes < 10 ? `0${minutes}` : minutes}:
-//         {seconds < 10 ? `0${seconds}` : seconds}
-//       </div>
-//       <button onClick={timerStatus}>{startTimer ? "Pause" : "Start"}</button>{" "}
-//       <button onClick={resetTimerStatus}>Reset</button>
-//     </div>
-//   );
-// };
-// export default Timer;
-
 import React, { useState, useEffect, useCallback } from "react";
+import { Link } from "react-router-dom";
 
 const CustomTimer = () => {
   const [timerState, setTimerState] = useState({
@@ -99,6 +49,8 @@ const CustomTimer = () => {
         {timerState.isRunning ? "Pause" : "Start"}
       </button>
       <button onClick={handleReset}>Reset</button>
+
+      <Link to="/">Countdown Timer</Link>
     </div>
   );
 };
