05f6693e1ac63d8e43eb786211ce88380d2d45b4
Ayudh <ayudhnandi@gmail.com>
2024-10-29 13:02:54 +0530

    added log in functionality

--- src/Components/LogIn.jsx
+++ src/Components/LogIn.jsx
@@ -1,21 +1,75 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
-import { signInWithEmailAndPassword } from "firebase/auth";
+import { Link, useNavigate } from "react-router-dom";
+import { signInWithEmailAndPassword, signOut } from "firebase/auth";
 import { auth } from "../auth/firebaseAuth";
 
 const LogIn = () => {
+  const Navigate = useNavigate();
   const [formInput, setFormInput] = useState({
-    email: "",
+    emailId: "",
     password: "",
   });
+  const [showPw, setShowPw] = useState(false);
 
-  const handleClicks = () => {
-    console.log("clicked");
+  const handleFormInput = (e) => {
+    setFormInput({
+      ...formInput,
+      [e.target.name]: e.target.value,
+    });
+  };
+  const handleShowPw = (e) => {
+    e.preventDefault();
+    setShowPw(!showPw);
+  };
+
+  const handleLogIn = async (e) => {
+    e.preventDefault();
+    // if (auth.user) {
+    //   console.log(auth);
+    //   await signOut(auth);
+    // }
+    await signInWithEmailAndPassword(
+      auth,
+      formInput.emailId,
+      formInput.password
+    )
+      .then((userCredential) => {
+        const user = userCredential.user;
+        console.log(user);
+        Navigate("/");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
   return (
     <>
-      <p>Log In</p>
-      <button onClick={handleClicks}>CLick</button>
+      <form>
+        <label htmlFor="emailId">Email ID</label>
+        <input
+          type="email"
+          placeholder="Email Id"
+          name="emailId"
+          onChange={handleFormInput}
+        ></input>
+        <label htmlFor="password">Password</label>
+        <input
+          type={showPw ? "text" : "password"}
+          placeholder="Password"
+          name="password"
+          onChange={handleFormInput}
+        ></input>
+        <button type="button" onClick={handleShowPw}>
+          Show Pw
+        </button>
+        <button type="submit" onClick={handleLogIn}>
+          Log In
+        </button>
+      </form>
+
+      <button>
+        <Link to="/">Home</Link>
+      </button>
     </>
   );
 };
