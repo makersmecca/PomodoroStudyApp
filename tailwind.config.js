b0fcf561a22c4c58b73532e838f16d086dcec22c
makersmecca <ayudhnandi@gmail.com>
2024-11-01 16:30:09 +0530

    added custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        backgroundColor: "#FFE5D9",
+        buttonColor: "#D95333",
+        accentColorLightCream: "#FFF7F0",
+        subtleSoftGrey: "#B0AFAF",
+        mutedOchre: "#FF8B42",
+        softOrange: "#FFA559",
+        gray: {
+          darkest: "#1f2d3d",
+          dark: "#3c4858",
+          DEFAULT: "#c0ccda",
+          light: "#e0e6ed",
+          lightest: "#f9fafc",
+        },
+      },
+    },
   },
   plugins: [],
 };
