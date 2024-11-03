c42978d7a04a4502e3f9f05b5329c6f5e666af82
makersmecca <ayudhnandi@gmail.com>
2024-11-03 18:06:40 +0530

    minor updates

--- src/Components/NavButtons.jsx
+++ src/Components/NavButtons.jsx
@@ -1,10 +1,10 @@
-const NavButtons = ({ componentName = "", currrentPage = false }) => {
+const NavButtons = ({ componentName = "", currentPage = false }) => {
   const buttonName = componentName;
   return (
     <button
       className={`py-3 px-2
       ${
-        currrentPage
+        currentPage
           ? "bg-backgroundColor hover:bg-red-300 border-2 border-buttonColor text-buttonColor"
           : "bg-buttonColor hover:bg-mutedOchre text-white"
       }  rounded-3xl active:scale-95 transition-all duration-300 text-lg shadow-md w-[100px]`}
