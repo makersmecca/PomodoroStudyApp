297fd7f539f8fbfd9bba577ea869471a53df0ad0
makersmecca <ayudhnandi@gmail.com>
2024-11-03 18:07:16 +0530

    added date time component to this component and updated slide in menu

--- src/Components/NavLinks.jsx
+++ src/Components/NavLinks.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
-
+import DisplayDateTime from "./DisplayDateTime";
 const NavLinks = () => {
   const [isOpen, setIsOpen] = useState(false);
 
@@ -8,61 +8,54 @@ const NavLinks = () => {
   const toggleMenu = () => {
     setIsOpen(!isOpen);
   };
-
+  // fixed md:top-20 top-10 right-4 md:right-20 z-20
   return (
     <>
       {/* Hamburger Icon */}
-      <div className="fixed md:top-20 top-10 right-4 md:right-20 z-20">
-        <button
-          onClick={toggleMenu}
-          className="text-gray-800 w-8 h-8 flex flex-col items-center justify-center focus:outline-none space-y-1"
-        >
-          {/* Icon for hamburger or close based on menu state */}
-          <span
-            className={`block w-8 h-1 bg-gray-800 rounded transition-transform duration-300 ${
-              isOpen ? "rotate-45 translate-y-2 w-7" : ""
-            }`}
-          ></span>
-          <span
-            className={`block w-8 h-1 bg-gray-800 rounded transition-opacity duration-300 ${
-              isOpen ? "opacity-0" : ""
-            }`}
-          ></span>
-          <span
-            className={`block w-8 h-1 bg-gray-800 rounded transition-transform duration-300 ${
-              isOpen ? "-rotate-45 -translate-y-2 w-7" : ""
-            }`}
-          ></span>
-        </button>
+      <div className="flex justify-between md:justify-end items-center fixed md:top-20 top-10 right-4 md:right-20 z-20 left-4 md:left-20">
+        {/* Time display - adjusted for widescreen */}
+        <div className="md:order-2 md:mr-20">
+          <DisplayDateTime></DisplayDateTime>
+        </div>
+
+        {/* Hamburger button */}
+        <div className="md:order-3">
+          <button
+            onClick={toggleMenu}
+            className="w-8 h-8 flex flex-col items-center justify-center focus:outline-none space-y-1"
+          >
+            <span
+              className={`block w-8 h-1 bg-buttonColor rounded transition-transform duration-300 ${
+                isOpen ? "rotate-45 translate-y-2 w-7" : ""
+              }`}
+            ></span>
+            <span
+              className={`block w-8 h-1 bg-buttonColor rounded transition-opacity duration-300 ${
+                isOpen ? "opacity-0" : ""
+              }`}
+            ></span>
+            <span
+              className={`block w-8 h-1 bg-buttonColor rounded transition-transform duration-300 ${
+                isOpen ? "-rotate-45 -translate-y-2 w-7" : ""
+              }`}
+            ></span>
+          </button>
+        </div>
       </div>
 
       {/* Slide-out Menu */}
       <div
-        className={`fixed top-12 right-0 md:w-1/4 w-2/3 bg-gray-200 shadow-lg transform transition-transform duration-300 ease-in-out z-10 ${
-          isOpen ? "translate-x-0" : "translate-x-full"
+        className={`fixed md:top-14 top-5 right-0 md:w-auto w-auto  bg-gray-200 shadow-lg transform transition-transform duration-300 ease-in-out z-10 rounded-3xl ${
+          isOpen ? "translate-x-0 md:me-4" : "translate-x-full"
         }`}
       >
-        <div className="flex flex-col items-start space-y-6 p-8 pt-20">
+        <div className="flex flex-col items-start md:items-center space-y-6 p-8 pt-20">
           <Link
             to="/customtimer"
             onClick={toggleMenu}
             className="text-lg text-gray-800 hover:text-blue-600"
           >
             Custom Timer
-          </Link>
-          <Link
-            to="/rest"
-            onClick={toggleMenu}
-            className="text-lg text-gray-800 hover:text-blue-600"
-          >
-            Rest
-          </Link>
-          <Link
-            to="/breathe"
-            onClick={toggleMenu}
-            className="text-lg text-gray-800 hover:text-blue-600"
-          >
-            Breathe
           </Link>
           <Link
             to="/todo"
