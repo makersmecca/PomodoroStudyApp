92cecb6f06d450bc9ec7b16c005c95e74f33f059
makersmecca <ayudhnandi@gmail.com>
2024-11-03 17:18:05 +0530

    component to add navigation butttons to move between the main pages

---
+++ src/Components/NavButtons.jsx
@@ -0,0 +1,16 @@
+const NavButtons = ({ componentName = "", currrentPage = false }) => {
+  const buttonName = componentName;
+  return (
+    <button
+      className={`py-3 px-2
+      ${
+        currrentPage
+          ? "bg-backgroundColor hover:bg-red-300 border-2 border-buttonColor text-buttonColor"
+          : "bg-buttonColor hover:bg-mutedOchre text-white"
+      }  rounded-3xl active:scale-95 transition-all duration-300 text-lg shadow-md w-[100px]`}
+    >
+      {buttonName}
+    </button>
+  );
+};
+export default NavButtons;
