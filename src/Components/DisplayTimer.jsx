dc1b73c3a1e65907a01049b75022b74677feac3d
Ayudh <ayudhnandi@gmail.com>
2024-11-04 16:57:41 +0530

    added fullscreen button to enter fullscreen

--- src/Components/DisplayTimer.jsx
+++ src/Components/DisplayTimer.jsx
@@ -42,116 +42,145 @@ const DisplayTimer = ({
           minimumMinutes: decrement,
         });
 
-  return (
-    <div className="min-h-screen flex flex-col items-center justify-center pt-0 sm:pt-20 px-4 sm:mt-10 lg:mt-0">
-      {/* Main container */}
-      <div className="relative w-full max-w-[300px] flex flex-col items-center gap-8">
-        <div className="flex gap-4">
-          <Link to="/">
-            <NavButtons
-              componentName={"Pomodoro"}
-              currentPage={location === "/"}
-            ></NavButtons>
-          </Link>
-          <Link to="/rest">
-            <NavButtons
-              componentName={"Rest"}
-              currentPage={location === "/rest"}
-            ></NavButtons>
-          </Link>
-          <Link to="/breathe">
-            <NavButtons
-              componentName={"Breathe"}
-              currentPage={location === "/breathe"}
-            ></NavButtons>
-          </Link>
-        </div>
+  const handleFullScreen = () => {
+    if (document.fullscreenElement) {
+      document.exitFullscreen();
+    } else {
+      document.documentElement.requestFullscreen();
+    }
+  };
 
-        {/* Circular timer container */}
-        <div className="relative w-[180px] xs:w-[200px] sm:w-[220px] md:w-[250px] aspect-square">
-          {/* Timer circle */}
-          <div className="absolute inset-0 bg-pastelYellow border-solid rounded-full shadow-lg flex items-center justify-center border-4 border-pastelOrange">
-            {/* Timer text container */}
-            <div className="flex flex-col items-center justify-center">
-              <div
-                className={`text-4xl xs:text-5xl sm:text-6xl font-semibold text-gray-800 mb-2 ${
-                  componentName === "Breathe" && "mt-9"
-                }`}
-              >
-                {timer.formatTime(timer.timeLeft)}
-              </div>
-              {componentName === "Breathe" ? (
-                <div>
-                  {timer.isRunning ? (
-                    <div className="text-base xs:text-lg sm:text-xl mt-2">
-                      {breatheState ? "Breathe In" : "Breathe Out"}
-                    </div>
-                  ) : (
-                    <div className="text-base xs:text-lg sm:text-xl mt-2 font-semibold">
-                      {" "}
-                      Breathe{" "}
-                    </div>
-                  )}
-                </div>
-              ) : (
-                ""
-              )}
-            </div>
+  return (
+    <>
+      <div className="min-h-screen flex flex-col items-center justify-center pt-0 sm:pt-20 px-4 sm:mt-10 lg:mt-0">
+        {/* Main container */}
+        <div className="relative w-full max-w-[300px] flex flex-col items-center gap-8">
+          <div className="flex gap-4">
+            <Link to="/">
+              <NavButtons
+                componentName={"Pomodoro"}
+                currentPage={location === "/"}
+              ></NavButtons>
+            </Link>
+            <Link to="/rest">
+              <NavButtons
+                componentName={"Rest"}
+                currentPage={location === "/rest"}
+              ></NavButtons>
+            </Link>
+            <Link to="/breathe">
+              <NavButtons
+                componentName={"Breathe"}
+                currentPage={location === "/breathe"}
+              ></NavButtons>
+            </Link>
           </div>
 
-          {/* Control buttons */}
-          <button
-            onClick={timer.handleDecrease}
-            className="absolute md:left-[-20%] left-[-30%] top-1/2 -translate-y-1/2 w-10 h-10 xs:w-12 xs:h-12 flex items-center justify-center bg-buttonColor text-white rounded-2xl hover:bg-opacity-80 active:scale-95 transition-all duration-300 text-3xl xs:text-2xl pb-1.5 shadow-md"
-          >
-            -
-          </button>
+          {/* Circular timer container */}
+          <div className="relative w-[180px] xs:w-[200px] sm:w-[220px] md:w-[250px] aspect-square">
+            {/* Timer circle */}
+            <div className="absolute inset-0 bg-pastelYellow border-solid rounded-full shadow-lg flex items-center justify-center border-4 border-pastelOrange">
+              {/* Timer text container */}
+              <div className="flex flex-col items-center justify-center">
+                <div
+                  className={`text-4xl xs:text-5xl sm:text-6xl font-semibold text-gray-800 mb-2 ${
+                    componentName === "Breathe" && "mt-9"
+                  }`}
+                >
+                  {timer.formatTime(timer.timeLeft)}
+                </div>
+                {componentName === "Breathe" ? (
+                  <div>
+                    {timer.isRunning ? (
+                      <div className="text-base xs:text-lg sm:text-xl mt-2">
+                        {breatheState ? "Breathe In" : "Breathe Out"}
+                      </div>
+                    ) : (
+                      <div className="text-base xs:text-lg sm:text-xl mt-2 font-semibold">
+                        {" "}
+                        Breathe{" "}
+                      </div>
+                    )}
+                  </div>
+                ) : (
+                  ""
+                )}
+              </div>
+            </div>
 
-          <button
-            onClick={timer.handleIncrease}
-            className="absolute md:right-[-20%] right-[-30%] top-1/2 -translate-y-1/2 w-10 h-10 xs:w-12 xs:h-12 flex items-center justify-center bg-buttonColor text-white rounded-2xl hover:bg-opacity-80 active:scale-95 transition-all duration-300 text-3xl xs:text-2xl pb-1 shadow-md"
-          >
-            +
-          </button>
-        </div>
-
-        {/* Timer controls */}
-        <div className="flex gap-4 w-full justify-center mt-4">
-          <button
-            onClick={timer.isRunning ? timer.handlePause : timer.handleStart}
-            className={`px-4 py-1 ${
-              timer.isRunning
-                ? "bg-orange-400 hover:bg-opacity-85"
-                : "bg-buttonColor hover:bg-opacity-85"
-            } text-white rounded-3xl active:scale-95 transition-all duration-300 text-xl shadow-md w-[90px]`}
-          >
-            {timer.isRunning ? "Pause" : "Start"}
-          </button>
-
-          <div
-            onClick={handleRotate}
-            className={`px-4 py-2 mt-1 text-buttonColor rounded-lg active:scale-95 transition-all duration-300 text-lg w-[50px] cursor-pointer`}
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="30"
-              height="30"
-              fill="currentColor"
-              className={`bi bi-arrow-clockwise stroke-current cursor-pointer ${
-                isRotating ? "animate-spinSlow" : ""
-              }`}
-              viewBox="0 0 16 16"
+            {/* Control buttons */}
+            <button
+              onClick={timer.handleDecrease}
+              className="absolute md:left-[-20%] left-[-30%] top-1/2 -translate-y-1/2 w-10 h-10 xs:w-12 xs:h-12 flex items-center justify-center bg-buttonColor text-white rounded-2xl hover:bg-opacity-80 active:scale-95 transition-all duration-300 text-3xl xs:text-2xl pb-1.5 shadow-md"
             >
-              <path
-                fillRule="evenodd"
-                d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2z"
-              />
-              <path d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466" />
-            </svg>
+              -
+            </button>
+
+            <button
+              onClick={timer.handleIncrease}
+              className="absolute md:right-[-20%] right-[-30%] top-1/2 -translate-y-1/2 w-10 h-10 xs:w-12 xs:h-12 flex items-center justify-center bg-buttonColor text-white rounded-2xl hover:bg-opacity-80 active:scale-95 transition-all duration-300 text-3xl xs:text-2xl pb-1 shadow-md"
+            >
+              +
+            </button>
+          </div>
+
+          {/* Timer controls */}
+          <div className="flex gap-4 w-full justify-center mt-4">
+            <button
+              onClick={timer.isRunning ? timer.handlePause : timer.handleStart}
+              className={`px-4 py-1 ${
+                timer.isRunning
+                  ? "bg-orange-400 hover:bg-opacity-85"
+                  : "bg-buttonColor hover:bg-opacity-85"
+              } text-white rounded-3xl active:scale-95 transition-all duration-300 text-xl shadow-md w-[90px]`}
+            >
+              {timer.isRunning ? "Pause" : "Start"}
+            </button>
+
+            <div
+              onClick={handleRotate}
+              className={`px-4 py-2 mt-1 text-buttonColor rounded-lg active:scale-95 transition-all duration-300 text-lg w-[50px] cursor-pointer`}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="30"
+                height="30"
+                fill="currentColor"
+                className={`bi bi-arrow-clockwise stroke-current cursor-pointer ${
+                  isRotating ? "animate-spinSlow" : ""
+                }`}
+                viewBox="0 0 16 16"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2z"
+                />
+                <path d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466" />
+              </svg>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+      {/* Fullscreen button */}
+      <button
+        onClick={handleFullScreen}
+        className="hidden md:block fixed bottom-4 right-5 md:right-20 p-3 transition-colors cursor-pointer hover:bg-pastelYellow rounded-full"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="25"
+          height="25"
+          fill="currentColor"
+          className="bi bi-arrows-fullscreen"
+          viewBox="0 0 16 16"
+        >
+          <path
+            fillRule="evenodd"
+            d="M5.828 10.172a.5.5 0 0 0-.707 0l-4.096 4.096V11.5a.5.5 0 0 0-1 0v3.975a.5.5 0 0 0 .5.5H4.5a.5.5 0 0 0 0-1H1.732l4.096-4.096a.5.5 0 0 0 0-.707m4.344 0a.5.5 0 0 1 .707 0l4.096 4.096V11.5a.5.5 0 1 1 1 0v3.975a.5.5 0 0 1-.5.5H11.5a.5.5 0 0 1 0-1h2.768l-4.096-4.096a.5.5 0 0 1 0-.707m0-4.344a.5.5 0 0 0 .707 0l4.096-4.096V4.5a.5.5 0 1 0 1 0V.525a.5.5 0 0 0-.5-.5H11.5a.5.5 0 0 0 0 1h2.768l-4.096 4.096a.5.5 0 0 0 0 .707m-4.344 0a.5.5 0 0 1-.707 0L1.025 1.732V4.5a.5.5 0 0 1-1 0V.525a.5.5 0 0 1 .5-.5H4.5a.5.5 0 0 1 0 1H1.732l4.096 4.096a.5.5 0 0 1 0 .707"
+          />
+        </svg>
+      </button>
+    </>
   );
 };
 
