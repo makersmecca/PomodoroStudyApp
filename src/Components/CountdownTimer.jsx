0e5f10ae7f68db3481e1d3b229f8f91556f1d6ce
Ayudh <ayudhnandi@gmail.com>
2024-11-04 12:04:49 +0530

    moved current user to hamburger and removed unused code

--- src/Components/CountdownTimer.jsx
+++ src/Components/CountdownTimer.jsx
@@ -1,52 +1,11 @@
-import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
-// import { auth } from "../auth/firebaseAuth";
-// import { onAuthStateChanged, getAuth } from "firebase/auth";
+import React from "react";
 import DisplayTimer from "./DisplayTimer";
-import CurrentUser from "./CurrentUser";
 import NavLinks from "./NavLinks";
 
 const CountdownTimer = () => {
-  //countdown timer logic
-  // const [currentUser, setCurrentUser] = useState(null);
-  // const timer = useTimerComp({
-  //   initialMinutes: 25,
-  //   incrementMinutes: 5,
-  //   minimumMinutes: 5,
-  // });
-
-  //firebase auth state check and sign out
-  // getAuth();
-  // useEffect(() => {
-  //   onAuthStateChanged(auth, (user) => {
-  //     if (user) {
-  //       console.log("User is signed in:", user.email);
-  //       setCurrentUser(user.email);
-  //     } else {
-  //       console.log("User is signed out");
-  //       setCurrentUser(null);
-  //     }
-  //   });
-  // }, []);
-
-  // const handleSignOut = () => {
-  //   auth
-  //     .signOut()
-  //     .then(() => {
-  //       console.log("User signed out successfully");
-  //       setCurrentUser(null);
-  //     })
-  //     .catch((error) => {
-  //       console.error("Error signing out:", error);
-  //     });
-  // };
   return (
     <div>
       <div className="flex justify-between w-full items-center">
-        <div className="flex items-center gap-4">
-          <CurrentUser></CurrentUser>
-        </div>
-
         <NavLinks></NavLinks>
       </div>
       <DisplayTimer
