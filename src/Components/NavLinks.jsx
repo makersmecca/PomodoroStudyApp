753b7ba7484c9d7e0654015b5d7664800af65828
Ayudh <ayudhnandi@gmail.com>
2024-11-04 12:06:12 +0530

    imported user context hook to track current user status

--- src/Components/NavLinks.jsx
+++ src/Components/NavLinks.jsx
@@ -1,8 +1,12 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
+import { UserContext } from "./UserContext";
 import { Link } from "react-router-dom";
 import DisplayDateTime from "./DisplayDateTime";
+import CurrentUser from "./CurrentUser";
 const NavLinks = () => {
   const [isOpen, setIsOpen] = useState(false);
+
+  const { currentUser } = useContext(UserContext);
 
   // Toggle menu open/close
   const toggleMenu = () => {
@@ -19,7 +23,7 @@ const NavLinks = () => {
         </div>
 
         {/* Hamburger button */}
-        <div className="md:order-3">
+        <div className="md:order-3 flex">
           <button
             onClick={toggleMenu}
             className="w-8 h-8 flex flex-col items-center justify-center focus:outline-none space-y-1"
@@ -45,11 +49,28 @@ const NavLinks = () => {
 
       {/* Slide-out Menu */}
       <div
-        className={`fixed md:top-14 top-5 right-0 md:w-auto w-auto  bg-gray-200 shadow-lg transform transition-transform duration-300 ease-in-out z-10 rounded-3xl ${
-          isOpen ? "translate-x-0 md:me-4" : "translate-x-full"
+        className={`fixed md:top-14 top-4 right-0 md:w-auto w-auto  bg-gray-200 shadow-lg transform transition-transform duration-300 ease-in-out z-10 rounded-3xl ${
+          isOpen ? "translate-x-0 me-2 md:me-4" : "translate-x-full"
         }`}
       >
-        <div className="flex flex-col items-start md:items-center space-y-6 p-8 pt-20">
+        <div className="flex flex-col items-start md:items-center space-y-6 md:px-6 px-8 pb-8 pt-7 md:pt-20">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="30"
+            height="30"
+            fill="currentColor"
+            className="bi bi-person-circle"
+            viewBox="0 0 16 16"
+          >
+            <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
+            <path
+              fillRule="evenodd"
+              d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"
+            />
+          </svg>
+          <div className="text-lg text-gray-800 hover:text-blue-600">
+            {currentUser ? currentUser : "Hello!"}
+          </div>
           <Link
             to="/customtimer"
             onClick={toggleMenu}
@@ -62,21 +83,7 @@ const NavLinks = () => {
             onClick={toggleMenu}
             className="text-lg text-gray-800 hover:text-blue-600"
           >
-            ToDo
-          </Link>
-          <Link
-            to="/stats"
-            onClick={toggleMenu}
-            className="text-lg text-gray-800 hover:text-blue-600"
-          >
-            Stats
-          </Link>
-          <Link
-            to="/settings"
-            onClick={toggleMenu}
-            className="text-lg text-gray-800 hover:text-blue-600"
-          >
-            Settings
+            Your Tasks
           </Link>
           <Link
             to="/SignUp"
@@ -85,13 +92,17 @@ const NavLinks = () => {
           >
             SignUp
           </Link>
-          <Link
-            to="/LogIn"
-            onClick={toggleMenu}
-            className="text-lg text-gray-800 hover:text-blue-600"
-          >
-            LogIn
-          </Link>
+          {currentUser ? (
+            <CurrentUser />
+          ) : (
+            <Link
+              to="/LogIn"
+              onClick={toggleMenu}
+              className="text-lg text-gray-800 hover:text-blue-600"
+            >
+              Log In
+            </Link>
+          )}
         </div>
       </div>
     </>
