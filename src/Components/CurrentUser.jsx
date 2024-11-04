996b54075fe7de6fc8e96f30e4b2a78bf4116af6
Ayudh <ayudhnandi@gmail.com>
2024-11-04 12:08:26 +0530

    used to handle signout only

--- src/Components/CurrentUser.jsx
+++ src/Components/CurrentUser.jsx
@@ -3,48 +3,17 @@ import { auth } from "../auth/firebaseAuth";
 import { useState, useEffect } from "react";
 
 const CurrentUser = () => {
-  const [currentUser, setCurrentUser] = useState(null);
   getAuth();
-  useEffect(() => {
-    onAuthStateChanged(auth, (user) => {
-      if (user) {
-        console.log("User is signed in:", user.email);
-        setCurrentUser(user.email.split("@")[0]);
-      } else {
-        console.log("User is signed out");
-        setCurrentUser(null);
-      }
-    });
-  }, []);
-
   const handleSignOut = () => {
     auth
       .signOut()
       .then(() => {
         console.log("User signed out successfully");
-        setCurrentUser(null);
       })
       .catch((error) => {
         console.error("Error signing out:", error);
       });
   };
-  // <>
-  //     {" "}
-  //     <div>
-  //       {currentUser != null ? (
-  //         <div className="flex items-center gap-2">
-  //           <span>
-  //             {currentUser}{" "}
-  //             <button type="button" onClick={handleSignOut}>
-  //               Sign Out
-  //             </button>
-  //           </span>
-  //         </div>
-  //       ) : (
-  //         ""
-  //       )}
-  //     </div>
-  //   </>
   return (
     <>
       <div
