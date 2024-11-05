55d68439f630d8bd035f0d9066094d29a02ca386
Ayudh <ayudhnandi@gmail.com>
2024-11-05 12:15:57 +0530

    updated navbar bg color

--- src/Components/NavLinks.jsx
+++ src/Components/NavLinks.jsx
@@ -22,9 +22,9 @@ const NavLinks = () => {
 
   const location = useLocation().pathname;
   return (
-    <>
+    <div className="">
       {/* Hamburger Icon */}
-      <div className="flex justify-between items-center fixed sm:absolute md:top-20 top-10 right-5 md:right-20 z-20 left-4 md:left-20">
+      <div className="flex justify-between items-center fixed sm:absolute md:top-20 top-10 right-5 md:right-20 z-20 left-4 md:left-20 bg-backgroundColor">
         {/* Time display - adjusted for widescreen */}
         <div className="md:order-first">
           <DisplayDateTime></DisplayDateTime>
@@ -140,7 +140,7 @@ const NavLinks = () => {
           )}
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
