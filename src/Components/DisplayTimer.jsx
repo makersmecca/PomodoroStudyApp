df1d14c557ade365330cab8591ab747fc6bf4fd2
makersmecca <ayudhnandi@gmail.com>
2024-11-07 00:34:51 +0530

    removed fullscreen handler button from here

--- src/Components/DisplayTimer.jsx
+++ src/Components/DisplayTimer.jsx
@@ -43,13 +43,6 @@ const DisplayTimer = ({
           minimumMinutes: decrement,
         });
 
-  const handleFullScreen = () => {
-    if (document.fullscreenElement) {
-      document.exitFullscreen();
-    } else {
-      document.documentElement.requestFullscreen();
-    }
-  };
   // ${
   //   breatheState
   //     ? "bg-green-100 shadow-green-500"
@@ -198,25 +191,6 @@ const DisplayTimer = ({
           </div>
         </div>
       </div>
-      {/* Fullscreen button */}
-      <button
-        onClick={handleFullScreen}
-        className="hidden md:block fixed bottom-4 right-5 md:right-20 p-3 transition-colors cursor-pointer hover:bg-pastelYellow rounded-full"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="25"
-          height="25"
-          fill="currentColor"
-          className="bi bi-arrows-fullscreen"
-          viewBox="0 0 16 16"
-        >
-          <path
-            fillRule="evenodd"
-            d="M5.828 10.172a.5.5 0 0 0-.707 0l-4.096 4.096V11.5a.5.5 0 0 0-1 0v3.975a.5.5 0 0 0 .5.5H4.5a.5.5 0 0 0 0-1H1.732l4.096-4.096a.5.5 0 0 0 0-.707m4.344 0a.5.5 0 0 1 .707 0l4.096 4.096V11.5a.5.5 0 1 1 1 0v3.975a.5.5 0 0 1-.5.5H11.5a.5.5 0 0 1 0-1h2.768l-4.096-4.096a.5.5 0 0 1 0-.707m0-4.344a.5.5 0 0 0 .707 0l4.096-4.096V4.5a.5.5 0 1 0 1 0V.525a.5.5 0 0 0-.5-.5H11.5a.5.5 0 0 0 0 1h2.768l-4.096 4.096a.5.5 0 0 0 0 .707m-4.344 0a.5.5 0 0 1-.707 0L1.025 1.732V4.5a.5.5 0 0 1-1 0V.525a.5.5 0 0 1 .5-.5H4.5a.5.5 0 0 1 0 1H1.732l4.096 4.096a.5.5 0 0 1 0 .707"
-          />
-        </svg>
-      </button>
     </>
   );
 };
