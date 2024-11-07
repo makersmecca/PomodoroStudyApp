be911d0ca85becdba28f84845774adc1cb30d941
makersmecca <ayudhnandi@gmail.com>
2024-11-08 01:22:50 +0530

    added state variable to handle timer states

--- src/Components/CountdownTimer.jsx
+++ src/Components/CountdownTimer.jsx
@@ -1,18 +1,24 @@
 import React from "react";
 import DisplayTimer from "./DisplayTimer";
 import NavLinks from "./NavLinks";
+import { useState } from "react";
 
 const CountdownTimer = () => {
+  const [timerState, setTimerState] = useState(false);
+  const handleTimerState = (timerState) => {
+    setTimerState(timerState);
+  };
   return (
     <div>
       <div className="flex justify-between w-full items-center">
-        <NavLinks></NavLinks>
+        <NavLinks timerState={timerState}></NavLinks>
       </div>
       <DisplayTimer
         defaultTime={25}
         increment={5}
         decrement={5}
         componentName="Pomodoro"
+        toggleTimerState={handleTimerState}
       ></DisplayTimer>
     </div>
   );
