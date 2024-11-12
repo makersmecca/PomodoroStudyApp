9b45944d3f5d218b86d496b280485cf71acd53a9
Ayudh <ayudhnandi@gmail.com>
2024-11-12 17:40:07 +0530

    added back button to go back

--- src/Components/navitems/ToDo.jsx
+++ src/Components/navitems/ToDo.jsx
@@ -208,9 +208,24 @@ const ToDo = () => {
           <div className="w-80 sm:w-10/12 max-w-md mt-52 rounded-lg shadow-md overflow-hidden">
             {" "}
             {/* Added mt-24 to account for fixed input */}
-            <h2 className="text-xl md:text-2xl text-center font-semibold p-4 bg-buttonColor text-white">
-              Your Tasks
-            </h2>
+            <div className="text-xl md:text-2xl flex justify-between items-center p-4 bg-buttonColor text-white">
+              <div>
+                <Link to="/">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="28"
+                    height="28"
+                    fill="currentColor"
+                    className="bi bi-arrow-left-circle-fill"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0m3.5 7.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5z" />
+                  </svg>
+                </Link>
+              </div>
+              <div className="flex-1 text-center">Your Tasks</div>
+              <div className="invisible">{"<-"}</div>
+            </div>
             {isLoading ? (
               <div className="flex justify-center items-center p-4">
                 <svg
