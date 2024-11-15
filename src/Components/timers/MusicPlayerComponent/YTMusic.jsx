c29c442be868af8a405a3bcae3e9966bf911989a
Ayudh Nandi <37550248+makersmecca@users.noreply.github.com>
2024-11-15 10:20:57 +0000

    added prev and next buttons as media controls

--- src/Components/timers/MusicPlayerComponent/YTMusic.jsx
+++ src/Components/timers/MusicPlayerComponent/YTMusic.jsx
@@ -39,18 +39,23 @@ const YTMusic = ({ status }) => {
     setYoutubeURL("");
   };
 
-  const play = player?.playVideo();
-  const pause = player?.pauseVideo();
-  const nextVideo = player?.nextVideo();
-  const prevVideo = player?.previousVideo();
+  const handleNext = () => {
+    player?.nextVideo();
+  };
+
+  const handlePrev = () => {
+    player?.previousVideo();
+  };
 
   const handlePlayPause = () => {
-    setIsPlaying((prevState) => !prevState);
-    if (isPlaying) {
-      pause();
-    } else {
-      play();
-    }
+    setIsPlaying((prevState) => {
+      if (isPlaying) {
+        player?.pauseVideo();
+      } else {
+        player?.playVideo();
+      }
+      return !prevState;
+    });
   };
 
   const trackTime = "20px"; //need to calculate this vlaue and assign it depending upon the track time
@@ -75,12 +80,25 @@ const YTMusic = ({ status }) => {
       {videoId || playlistId ? (
         <YouTube videoId={videoId} opts={opts} onReady={onReady} />
       ) : null}
+
       <div className="mt-8">
         <div className="mb-2">Media not playing</div>
         <div className={`bg-buttonColor pt-0.5 w-[${trackTime}]`}></div>
-        <div className="flex justify-center h-[40pxpx] w-full items-center mt-4">
-          <button onClick={handlePlayPause} className="self-center">
-            {isPlaying ? (
+        <div className="flex justify-center h-[40pxpx] w-full items-center mt-4 gap-2">
+          <button onClick={handlePrev} className="hover:scale-95">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="35"
+              height="35"
+              fill="currentColor"
+              className="bi bi-skip-start-circle-fill ps-1 md:ps-0 md:pe-0.5"
+              viewBox="0 0 16 16"
+            >
+              <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M9.71 5.093 7 7.028V5.5a.5.5 0 0 0-1 0v5a.5.5 0 0 0 1 0V8.972l2.71 1.935a.5.5 0 0 0 .79-.407v-5a.5.5 0 0 0-.79-.407" />
+            </svg>
+          </button>
+          {isPlaying ? (
+            <button onClick={handlePlayPause} className="hover:scale-95">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="35"
@@ -91,7 +109,9 @@ const YTMusic = ({ status }) => {
               >
                 <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M6.25 5C5.56 5 5 5.56 5 6.25v3.5a1.25 1.25 0 1 0 2.5 0v-3.5C7.5 5.56 6.94 5 6.25 5m3.5 0c-.69 0-1.25.56-1.25 1.25v3.5a1.25 1.25 0 1 0 2.5 0v-3.5C11 5.56 10.44 5 9.75 5" />
               </svg>
-            ) : (
+            </button>
+          ) : (
+            <button onClick={handlePlayPause} className="hover:scale-95">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="35"
@@ -102,7 +122,19 @@ const YTMusic = ({ status }) => {
               >
                 <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M6.79 5.093A.5.5 0 0 0 6 5.5v5a.5.5 0 0 0 .79.407l3.5-2.5a.5.5 0 0 0 0-.814z" />
               </svg>
-            )}
+            </button>
+          )}
+          <button onClick={handleNext} className="hover:scale-95">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="35"
+              height="35"
+              fill="currentColor"
+              className="bi bi-skip-end-circle-fill ps-1 md:ps-0 md:pe-0.5"
+              viewBox="0 0 16 16"
+            >
+              <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M6.79 5.093A.5.5 0 0 0 6 5.5v5a.5.5 0 0 0 .79.407L9.5 8.972V10.5a.5.5 0 0 0 1 0v-5a.5.5 0 0 0-1 0v1.528z" />
+            </svg>
           </button>
         </div>
       </div>
