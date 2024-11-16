6dc5e788d7ae9f2310740fe95cbc18f5451215cb
makersmecca <ayudhnandi@gmail.com>
2024-11-16 19:31:48 +0530

    added scroll text animation config

--- tailwind.config.js
+++ tailwind.config.js
@@ -32,11 +32,16 @@ export default {
       animation: {
         spinSlow: "spin 1s ease-in-out",
         breatheGlow: "breatheGlow 10s ease-in-out infinite",
+        scrollText: "scrollText 5s linear infinite",
       },
       keyframes: {
         breatheGlow: {
           "0%, 100%": { boxShadow: "0 0 5px 0 rgba(201, 104, 104, 0.3)" },
           "50%": { boxShadow: "0 0 20px 10px rgba(201, 104, 104, 0.7)" },
+        },
+        scrollText: {
+          "0%": { transform: "translateX(10%)" },
+          "100%": { transform: "translateX(-100%)" },
         },
       },
 
