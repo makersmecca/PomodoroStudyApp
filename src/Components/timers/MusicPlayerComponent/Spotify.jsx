f0d4c305532bef6f5c2106fb3b2097bd12560c51
Ayudh Nandi <37550248+makersmecca@users.noreply.github.com>
2024-11-19 06:34:16 +0000

    added spotify auth to authenticate users for media playback

--- src/Components/timers/MusicPlayerComponent/Spotify.jsx
+++ src/Components/timers/MusicPlayerComponent/Spotify.jsx
@@ -1,4 +1,49 @@
+import { useEffect, useState } from "react";
 const Spotify = () => {
-  return <>Spotify Player</>;
+  const CLIENT_ID = "e64dced41cb248d89c48229a4e8c2624";
+  const REDIRECT_URI =
+    "https://bug-free-space-train-pg6rw956pvxfrvqx-5173.app.github.dev/";
+  const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
+  const RESPONSE_TYPE = "token";
+
+  const [token, setToken] = useState("");
+  useEffect(() => {
+    const hash = window.location.hash;
+    let token = window.localStorage.getItem("token");
+
+    if (!token && hash) {
+      token = hash
+        .substring(1)
+        .split("&")
+        .find((elem) => elem.startsWith("access_token"))
+        .split("=")[1];
+
+      window.location.hash = "";
+      window.localStorage.setItem("token", token);
+    }
+
+    setToken(token);
+  }, []);
+
+  const logout = () => {
+    setToken("");
+    window.localStorage.removeItem("token");
+  };
+
+  return (
+    <>
+      <div>Spotify Player</div>
+      {!token ? (
+        <a
+          href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}
+        >
+          Login to Spotify
+        </a>
+      ) : (
+        <button onClick={logout}>Logout</button>
+      )}
+      ;
+    </>
+  );
 };
 export default Spotify;
