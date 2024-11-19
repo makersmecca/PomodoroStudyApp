efc48c9dee96635fd5c010c0e3881e6c6a0b016a
Ayudh Nandi <37550248+makersmecca@users.noreply.github.com>
2024-11-19 07:27:29 +0000

    trying to integrate soundcloud

---
+++ src/Components/timers/MusicPlayerComponent/SoundCloud.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+const SoundCloud = () => {
+  const [searchInput, setSearchInput] = useState("");
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+  };
+
+  const handleSearchInput = (e) => {
+    setSearchInput(e.target.value);
+  };
+  return (
+    <div className="mb-3">
+      <div className="text-lg font-semibold mb-2 text-center bg-buttonColor text-white rounded-xl w-full cursor-default">
+        SoundCloud
+      </div>
+      <form onSubmit={handleSearch}>
+        <input
+          type="text"
+          placeholder="Search"
+          onChange={handleSearchInput}
+          value={searchInput}
+        />
+        <button type="submit">Search</button>
+      </form>
+    </div>
+  );
+};
+export default SoundCloud;
